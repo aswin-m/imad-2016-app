4123ea56038d923bae4e4639f511e50894bc60e6
ASWIN DAS-M <aswindasachuz@gmail.com>
2016-09-29 16:19:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/ui/style.css', function (req, res) {
-    
-app.get('/article-one'), function (req, res) {
+    app.get('/article-one'), function (req, res) {
     res.send('article-one requested and will be served here');
 };   
     
