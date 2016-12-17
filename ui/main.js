d557e2a65fb4f29591be5ff404954e591224d121
ASWIN DAS-M <aswindasachuz@gmail.com>
2016-12-17 16:12:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,14 +3,25 @@ var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick = function () {
-    
+   
+    //Create a request oobject 
     // Make a request to the counter endpoint
     var request = new XMLHttpRequest();
     
+    
    // capture the response and store it in a variable
-   
-   // Render the variale in the correct span
-   counter = counter + 1;
-   var span = document.getElementById('count');
-   span.innerHTML = counter.toString();
+   request.onreadystatechange = function() {
+     if (request.readyState === XMLHttprequest.DONE) {
+         // Take some action
+         if (request.status === 200) {
+           var counter = request.responseText;
+           var span = document.getElementById('count');
+           span.innerHTML = counter.toString()  ;
+           }     
+     }
+     //Not done yet
+   };
+   // Make the request
+   request.open('GET', 'http://http://aswin-m.imad.hasura-app.io/counter',true);
+   request.send(null);
 };
