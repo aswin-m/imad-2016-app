14b4cdd77d76dab05d4db0324b73e0519c7756a8
ASWIN DAS-M <aswindasachuz@gmail.com>
2016-12-22 17:08:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ button.onclick = function () {
          if (request.status === 200) {
            var counter = request.responseText;
            var span = document.getElementById('count');
-           span.innerHTML = counter.toString()  ;
+           span.innerHTML = counter.toString();
            }     
      }
      //Not done yet
