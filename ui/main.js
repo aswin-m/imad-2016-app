523c3a900e4905ff87cf39407c511cf66dd6aae4
ASWIN DAS-M <aswindasachuz@gmail.com>
2016-12-31 21:59:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,16 +13,21 @@ button.onclick = function () {
      if (request.readyState === XMLHttprequest.DONE) {
          // Take some action
          if (request.status === 200) {
-           var counter = request.responseText;
-           var span = document.getElementById('count');
-           span.innerHTML = counter.toString();
+         // capture a list of names and render it as a list
+         var names = request.responseText;
+         names = JSON.parse(names);
+         var list = '';
+          for (var i=0; i< names.length; i++) {
+            list += '<li>' + names[i] + '</li>';
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+    
            }     
      }
      //Not done yet
    };
-   // Make the request
-   request.open('GET', 'http://http://aswin-m.imad.hasura-app.io/counter',true);
-   request.send(null);
+   
 };
 
 // submit name
@@ -32,13 +37,9 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     // Make a request to the server and  send the name
     
-    // capture a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for (var i=0; i< names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-    };   
-    
+  // Make the request
+   request.open('GET', 'http://http://aswin-m.imad.hasura-app.io/submit-name?name=' + name,true);
+   request.send(null);
+   
+};   
+   
