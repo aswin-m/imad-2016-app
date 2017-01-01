b0ab82ad96d578e1fbc9e2178bfc07051d74101b
ASWIN DAS-M <aswindasachuz@gmail.com>
2017-01-01 13:18:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,14 +29,30 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    // Make a request to the server and send the name 
     
-    // capture a list of name and render it  as a list
-  var names = ['name1, name2, name3, name4'];
-  var list = '';
-  for (var i=0; i< names.length; i++){
-      list += '<li>' + names[1] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+        // create a request object
+    var request = new XMLHttpRequest();
+    
+    // capture the response and store it  in a variable 
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Take some action 
+            if (request.status === 200) {
+                  // capture a list of name and render it  as a list
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for (var i=0; i< names.length; i++){
+                list += '<li>' + names[1] + '</li>';
+            }
+            var ul = document.getElementById('namelist');
+             ul.innerHTML = list;
+            }
+        }
+        // Not done yet
+    };
+    
+    // Make the request
+    request.open('GET', 'http://aswin-m.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
 };
