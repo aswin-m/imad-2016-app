daa615a643192fbaa46978b18542a1e2d010d73a
ASWIN DAS-M <aswindasachuz@gmail.com>
2017-01-01 12:56:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,46 +1,42 @@
-//countee code
+// counter code
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function () {
-   
-    // Create a request object
+    
+    // create a request object
     var request = new XMLHttpRequest();
     
-    
-   // capture the response and store it in a variable
-   request.onreadystatechange = function() {
-     if (request.readyState === XMLHttprequest.DONE) {
-         // Take some action
-         if (request.status === 200) {
-         // capture a list of names and render it as a list
-         var names = request.responseText;
-         names = JSON.parse(names);
-         var list = '';
-          for (var i=0; i< names.length; i++) {
-            list += '<li>' + names[i] + '</li>';
+    // capture the response and store it  in a variable 
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Take some action 
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
             }
-            var ul = document.getElementById('namelist');
-            ul.innerHTML = list;
+        }
+        // Not done yet
+    };
     
-           }     
-     }
-     //Not done yet
-   };
-   
+    // Make the request
+    request.open('GET', 'http://aswin-m.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
 
-// submit name
-
+// submit name 
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    // Make a request to the server and  send the name
+    // Make a request to the server and send the name 
     
-  // Make the request
-  var nameInput = document.getElementById('name');
-  var name = nameInput.value;
-   request.open('GET', 'http://http://aswin-m.imad.hasura-app.io/submit-name?name=' + name,true);
-   request.send(null);
-   
-};   
-   
+    // capture a list of name and render it  as a list
+  var names = ['name1, name2, name3, name4'];
+  var list = '';
+  for (var i=0; i< names.length; i++){
+      list += '<li>' + names[1] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+};
